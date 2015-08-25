858a4e990f8078792aecfc0ecabf11f303271836
Espen Moe-Nilssen <espen@medialog.no>
2015-08-25 17:47:02 +0200

    Phone 5 theme

--- medialog/fullpagetheme/interfaces.py
+++ medialog/fullpagetheme/interfaces.py
@@ -1,6 +1,6 @@
 from zope.interface import Interface
 
 
-class ICustomTheme(Interface):
+class IFullpageTheme(Interface):
     """Marker interface that defines a Zope 3 browser layer.
     """
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2.1'
+version = '0.3'
 
 setup(name='medialog.fullpagetheme',
       version=version,
@@ -27,6 +27,7 @@ setup(name='medialog.fullpagetheme',
       install_requires=[
           'setuptools',
           'z3c.jbot',
+          'medialog.bergensiana',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
