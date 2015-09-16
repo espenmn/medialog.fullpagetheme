9c85a8f16fddb23685c8b34c82858912bf6d92ec
Espen Moe-Nilssen <espen@medialog.no>
2015-09-16 22:02:06 +0200

    menu anchors in place

--- medialog/fullpagetheme/interfaces.py
+++ medialog/fullpagetheme/interfaces.py
@@ -3,4 +3,8 @@ from zope.interface import Interface
 
 class IFullpageTheme(Interface):
     """Marker interface that defines a Zope 3 browser layer.
+    This interface is referred in browserlayer.xml.
+    All views and viewlets register against this layer will appear on
+    your Plone site only when the add-on installer has been run,
+    for example: full folder view (all content)
     """
